90a55da3e8ae43a8a17b21e0f4be3d86bc55e9ec
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-01-01 02:37:42 +0100

    Fix the float comparison in stft test.

--- tests/stftModel_test.py
+++ tests/stftModel_test.py
@@ -26,8 +26,8 @@ def test_reconstruct_sound():
     assert expected_frame_count == len(phase_spectrogram)
 
     # statistics of the spectrogram for regression testing without explicitly storing the whole data
-    assert -102.86187076588583 == np.mean(mag_spectrogram)
-    assert 11.368333745102881 == np.mean(phase_spectrogram)
+    assert np.allclose(-102.86187076588583, np.mean(mag_spectrogram))
+    assert np.allclose(11.368333745102881, np.mean(phase_spectrogram))
 
     # TODO: should be the same as len(x)
     assert expected_frame_count * hop_size == len(x_reconstructed)
